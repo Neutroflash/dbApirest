d059ba8bd85bb0209ceb9db97399e8384e98ceee
Neutroflash <neutroflashp@hotmail.com>
2024-05-03 17:54:33 -0500

    add port

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
     user: "postgres",
-    host: "localhost",
+    host: "34.29.62.6",
     database: "postgres",
-    password: "pass",
-    port: 5467
+    password: "1234",
+    port: 5432
 })
 
 module.exports = pool
