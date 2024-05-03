58bf412089ce1bb5b66300820d2f7d023e2d767d
Neutroflash <neutroflashp@hotmail.com>
2024-05-03 18:51:36 -0500

    port

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
     user: "postgres",
-    host: "34.29.62.6",
+    host: "127.0.0.1",
     database: "postgres",
     password: "1234",
     port: 5432
