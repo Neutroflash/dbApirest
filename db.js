96f71e4053700dc6bd95dc7f547fd4191ec18916
Neutroflash <neutroflashp@hotmail.com>
2024-05-03 20:42:01 -0500

    change router

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    host: "127.0.0.1",
-    database: "postgres",
-    password: "1234",
-    port: 5432
+    user: "ucmgffbmfio5ip",
+    host: "cf9gid2f6uallg.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com",
+    database: "d6afqe0he79duk",
+    password: "p06ae7e8f0bef01090bc57879c423c10da1d5fa5c9f6851090eebb39ec9e6f9aa",
+    port: 5432,
+    ssl: { sslmode: 'require', rejectUnauthorized: false }
 })
 
 module.exports = pool
